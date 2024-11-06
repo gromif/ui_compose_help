9330fe7f051c1508a287c0ba04589494e1a86b43
Nevidimka655 <alex.kobrys@gmail.com>
2024-11-06 11:42:01 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.nevidimka655.compose_help"
+    compileSdk = project.property("compileSdk").toString().toInt()
+
+    defaultConfig {
+        minSdk = project.property("minSdk").toString().toInt()
+        consumerProguardFiles("consumer-rules.pro")
+
+        buildFeatures.run {
+            compose = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    api(project(mapOf("path" to ":ui:compose-core")))
+}
---
+++ src/main/kotlin/com/nevidimka655/compose_help/Help.kt
@@ -0,0 +1,13 @@
+package com.nevidimka655.compose_help
+
+object Help {
+
+    /*object Builder {
+        
+        fun add(title: TextWrap, text: TextWrap) {
+            
+        }
+        
+    }*/
+
+}
